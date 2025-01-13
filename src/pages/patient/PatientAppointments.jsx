52038febb39dfa9e37966f8fec68622b5e219979
AndriuuU <andresitomolinaclan@gmail.com>
2025-01-13 09:45:54 +0100

    Appointment basic

--- src/pages/patient/PatientAppointments.jsx
+++ src/pages/patient/PatientAppointments.jsx
@@ -1,8 +1,10 @@
 //import React from 'react'
 
+import AppointmentForm from "../../components/AppointmentForm/AppointmentForm"
+
 const PatientAppointments = () => {
   return (
-    <div>PatientAppointments</div>
+    <AppointmentForm />
   )
 }
 
