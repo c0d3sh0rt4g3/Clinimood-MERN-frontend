8765772d9ad1bfe286566ede9fdd41d85f92e489
AndriuuU <andresitomolinaclan@gmail.com>
2025-01-23 13:11:40 +0100

    test navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
-//import React from 'react'
+// import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import "../style/components/Navbar.scss";
+//import {Logo} from "../../public/Logo.png";
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <header className="navbar-header">
-        <img src="" alt="" />
+      <header className="navbar__header">
+        <div className="navbar__logo">
+          <img src="/Logo.png" alt="Clinimood Logo" className="navbar__logo-image" />
+          <span className="navbar__logo-text">Clinimood</span>
+        </div>
         <ul className="navbar__links">
-        <NavLink to="/"> Home</NavLink>
-        <NavLink to="/login"> Login</NavLink>
-        <NavLink to="/register"> Register</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/history">History</NavLink>
-        <NavLink to="/new-appointment"> New appointment</NavLink>
-        <NavLink to="/doctors">Doctors</NavLink>
+          <NavLink to="/" className="navbar__link">Home</NavLink>
+          <NavLink to="/login" className="navbar__link">Login</NavLink>
+          <NavLink to="/register" className="navbar__link">Register</NavLink>
+          <NavLink to="/contact" className="navbar__link">Contact</NavLink>
+          <NavLink to="/history" className="navbar__link">History</NavLink>
+          <NavLink to="/new-appointment" className="navbar__link">New appointment</NavLink>
+          <NavLink to="/doctors" className="navbar__link">Doctors</NavLink>
         </ul>
-    </header>
-</nav>
+        <div className="navbar__actions">
+          <button className="navbar__button navbar__button--primary">Book an appointment</button>
+        </div>
+      </header>
+    </nav>
   )
 }
 
