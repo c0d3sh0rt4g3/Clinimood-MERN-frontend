4fb0abaa5b168575a0bd16ed29d7f4d1ed8d51ca
AndriuuU <andresitomolinaclan@gmail.com>
2025-01-24 12:46:47 +0100

    resolver

--- src/components/Appointments/AppointmentsHistorialPage.jsx
+++ src/components/Appointments/AppointmentsHistorialPage.jsx
@@ -8,7 +8,7 @@ import {
   isBefore,
   isToday,
 } from "date-fns";
-import "../../style/main.scss";
+import "../../style/components/AppointmentsHistorialPage.scss";
 
 const formatAppointmentDate = (date) => format(new Date(date), "yyyy-MM-dd");
 
