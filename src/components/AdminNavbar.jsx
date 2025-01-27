a58ca132631f5748968e2b5d0bccf74b350e953b
out0f4mm0 <eriking2004@gmail.com>
2025-01-27 10:46:09 +0100

    Added navbar actions on adminNavbar

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import useAuthStore from "../context/useAuthStore.jsx"
-import {useNavigate} from "react-router-dom"
+import {NavLink, useNavigate} from "react-router-dom"
 
 const AdminNavbar = () => {
+    const [darkMode, setDarkMode] = useState(
+    localStorage.getItem('darkMode') === 'enabled'
+    );
     const { clearUser } = useAuthStore()
     const navigate = useNavigate()
 
@@ -11,12 +14,30 @@ const AdminNavbar = () => {
         clearUser()
         navigate('/')
     }
+    // Toggle dark mode
+      const toggleDarkMode = () => {
+        const newMode = !darkMode;
+        setDarkMode(newMode);
+        document.body.classList.toggle('dark-mode', newMode);
+        localStorage.setItem('darkMode', newMode ? 'enabled' : 'disabled');
+      };
+
     return (
         <header>
             <ul className={"navbar-links"}>
                 <h2 id={"user-name"}>Admin</h2>
-                <button onClick={handleLogout}>Sign out</button>
             </ul>
+            <div className="navbar__actions">
+              <button onClick={toggleDarkMode} className="navbar__button navbar__button--mode">
+                {darkMode ? '☀️' : '🌙'}
+              </button>
+              <button onClick={handleLogout} className="navbar__button navbar__button--logout">
+                Logout
+              </button>
+              <NavLink to="/profile" className="navbar__profile">
+                <img src="/profile-icon.png" alt="Profile" className="navbar__profile-icon" />
+              </NavLink>
+            </div>
         </header>
     )
 }
