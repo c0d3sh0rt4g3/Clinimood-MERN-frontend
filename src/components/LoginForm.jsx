6ff3a23473e498111a49e849269ccf5f86c6ee44
AlvaroRui <aruiz@iesrafaelalberti.es>
2025-01-31 09:57:10 +0100

    Dark Mode and styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,6 +4,7 @@ import { TextField, Button } from '@mui/material'
 import {NavLink, useNavigate} from 'react-router-dom'
 import useAuthStore from "../context/useAuthStore.jsx"
 import {useEffect} from "react";
+import "../style/components/Forms.scss"
 
 const validationSchema = Yup.object({
     email: Yup.string().email('Email not valid').required('Email is required'),
@@ -46,56 +47,71 @@ const LoginForm = () => {
 
 
     return (
-        <Formik
-            initialValues={{ email: '', password: '' }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-        >
-            {({ isSubmitting, handleSubmit }) => (
-                <form className="auth-form-container" onSubmit={handleSubmit}>
-                    <h1>LOGIN</h1>
+        <div className="form-container login-page"> {/* Contenedor principal con estilos generales */}
+            <div className="form-box"> {/* Caja con sombras, padding y animación */}
+                <h1>LOGIN</h1>
 
-                    <Field name="email">
-                        {({ field }) => (
-                            <TextField
-                                {...field}
-                                type="email"
-                                label="Email"
-                                variant="outlined"
-                                margin="normal"
-                                fullWidth
-                            />
-                        )}
-                    </Field>
-                    <ErrorMessage name="email" component="div" className="error" />
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ isSubmitting, handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-field"> {/* Aplicando estilos a los inputs */}
+                                <Field name="email">
+                                    {({ field }) => (
+                                        <TextField
+                                            {...field}
+                                            type="email"
+                                            label="Email"
+                                            variant="outlined"
+                                            margin="normal"
+                                            fullWidth
+                                        />
+                                    )}
+                                </Field>
+                                <ErrorMessage name="email" component="div" className="error-message" />
+                            </div>
 
-                    <Field name="password">
-                        {({ field }) => (
-                            <TextField
-                                {...field}
-                                type="password"
-                                label="Password"
-                                variant="outlined"
-                                margin="normal"
+                            <div className="input-field">
+                                <Field name="password">
+                                    {({ field }) => (
+                                        <TextField
+                                            {...field}
+                                            type="password"
+                                            label="Password"
+                                            variant="outlined"
+                                            margin="normal"
+                                            fullWidth
+                                        />
+                                    )}
+                                </Field>
+                                <ErrorMessage name="password" component="div" className="error-message" />
+                            </div>
+
+                            <p className="form-links">
+                                You don´t have an account? <NavLink to="/register">Create one!</NavLink>
+                            </p>
+                            <p className="form-links">
+                                Did you forget your password? <NavLink to="/password-recovery">Click here</NavLink>
+                            </p>
+
+                            <Button
+                                type="submit"
+                                className="form-button"
+                                variant="contained"
+                                disabled={isSubmitting}
                                 fullWidth
-                            />
-                        )}
-                    </Field>
-                    <ErrorMessage name="password" component="div" className="error" />
-                    <p>You dont have an account? <NavLink to="/register">Create one!</NavLink></p>
-                    <p>Did you forget your password? <NavLink to="/password-recovery">Click here</NavLink></p>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        disabled={isSubmitting}
-                        fullWidth
-                    >
-                        {isSubmitting ? '...' : 'Login'}
-                    </Button>
-                </form>
-            )}
-        </Formik>
-    )
+                            >
+                                {isSubmitting ? '...' : 'Login'}
+                            </Button>
+                        </form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
 }
 
 export default LoginForm
